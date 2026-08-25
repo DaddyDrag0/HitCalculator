23cdc96b5042e6845fc5235aaa59a5bce6ca824e
Drago <dragoidc3@gmail.com>
2026-08-24 23:27:43 -0400

    Update Hit Calc

---
+++ roll-sim-pack-loader-v20.js
@@ -0,0 +1,13 @@
+(() => {
+  function loadPackUi() {
+    if (!document.getElementById('rollSimulatorV15')) return;
+    if (document.querySelector('script[data-roll-sim-pack-ui-v19]')) return;
+    const script = document.createElement('script');
+    script.src = './roll-sim-pack-ui-v19.js?rev=20260824-2004';
+    script.dataset.rollSimPackUiV19 = '1';
+    document.head.append(script);
+  }
+
+  if (document.readyState === 'complete') loadPackUi();
+  else window.addEventListener('load', loadPackUi, { once: true });
+})();
