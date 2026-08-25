25bc3059be8082105f9004097d0e76c5fe155577
Drago <dragoidc3@gmail.com>
2026-08-25 00:20:37 -0400

    Update Hit Calc

--- roll-sim-simplify-loader-v22.js
+++ roll-sim-simplify-loader-v22.js
@@ -2,12 +2,21 @@
   if (window.__rollSimSimplifyLoaderV22) return;
   window.__rollSimSimplifyLoaderV22 = true;
 
+  function loadRunUI() {
+    if (document.querySelector('script[data-roll-sim-run-ui-v24]')) return;
+    const ui = document.createElement('script');
+    ui.src = './roll-sim-run-ui-v24.js?rev=20260825-0016';
+    ui.dataset.rollSimRunUiV24 = '1';
+    document.head.append(ui);
+  }
+
   function load() {
     if (!document.getElementById('rollSimulatorV15')) return false;
     if (document.querySelector('script[data-roll-sim-simplify-v22]')) return true;
     const script = document.createElement('script');
     script.src = './roll-sim-simplify-v21.js?rev=20260824-2358';
     script.dataset.rollSimSimplifyV22 = '1';
+    script.addEventListener('load', loadRunUI, { once: true });
     document.head.append(script);
     return true;
   }
