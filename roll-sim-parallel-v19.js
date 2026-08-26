11350c4a7060095dfffa4e9c7ed470263789beb8
Drago <dragoidc3@gmail.com>
2026-08-26 16:05:29 -0400

    Update Hit Calc

--- roll-sim-parallel-v19.js
+++ roll-sim-parallel-v19.js
@@ -199,7 +199,7 @@
       };
 
       for (let slot = 0; slot < concurrency; slot += 1) {
-        const worker = new NativeWorker('./roll-sim-worker-v30.js?rev=20260825-1756');
+        const worker = new NativeWorker('./roll-sim-worker-v30.js?rev=20260826-1602');
         this._workers.push(worker);
         worker.addEventListener('message', (event) => {
           if (failed || this._cancelled) return;
