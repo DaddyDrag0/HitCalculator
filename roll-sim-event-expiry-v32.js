606b0c427d509a74be23561d002732415a408b7b
Drago <dragoidc3@gmail.com>
2026-08-26 16:22:46 -0400

    Update Hit Calc

--- roll-sim-event-expiry-v32.js
+++ roll-sim-event-expiry-v32.js
@@ -5,9 +5,11 @@
   const expired = new Set(['Fate Seamstress', 'Eonus', 'Eclipseborn Luminant']);
   for (const card of data.cards) {
     if (!expired.has(card?.name)) continue;
+    if (!Number.isFinite(card.expiredBaseRarity)) card.expiredBaseRarity = Number(card.rarity) || 0;
     card.currentEvent = false;
     card.rollable = false;
     card.expiredEvent = true;
+    card.rarity = Number.POSITIVE_INFINITY;
   }
 
   if (Array.isArray(data.currentEvents)) data.currentEvents.splice(0, data.currentEvents.length);
