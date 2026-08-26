c1474057b16fb800e3ab1dadf4db56f7b08fcfbf
Drago <dragoidc3@gmail.com>
2026-08-26 16:04:57 -0400

    Update Hit Calc

--- roll-sim-worker-v30.js
+++ roll-sim-worker-v30.js
@@ -1,4 +1,4 @@
-importScripts('./roll-sim-worker-v19.js?rev=20260825-1756');
+importScripts('./roll-sim-worker-v19.js?rev=20260826-1602');
 
 // High-end RNG fix:
 // - Cards are sampled from the exact sequential card distribution.
