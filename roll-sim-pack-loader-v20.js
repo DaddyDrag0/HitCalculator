0591008fa285ade0167165af2ecbf9bcaba2fb81
Drago <dragoidc3@gmail.com>
2026-08-26 22:31:06 -0400

    Refresh card pack layout loader

--- roll-sim-pack-loader-v20.js
+++ roll-sim-pack-loader-v20.js
@@ -3,7 +3,7 @@
     if (!document.getElementById('rollSimulatorV15')) return;
     if (document.querySelector('script[data-roll-sim-pack-ui-v19]')) return;
     const script = document.createElement('script');
-    script.src = './roll-sim-pack-ui-v19.js?rev=20260826-1648';
+    script.src = './roll-sim-pack-ui-v19.js?rev=20260826-2248';
     script.dataset.rollSimPackUiV19 = '1';
     document.head.append(script);
   }
