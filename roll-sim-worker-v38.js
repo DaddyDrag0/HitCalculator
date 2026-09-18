c1cc347cbe6459652e99e6ec4e48558c8b770965
Drago <dragoidc3@gmail.com>
2026-09-18 01:05:21 -0400

    Refresh roll sim v30 dependency

--- roll-sim-worker-v38.js
+++ roll-sim-worker-v38.js
@@ -1,4 +1,4 @@
-importScripts('./roll-sim-worker-v30.js?rev=20260826-1648');
+importScripts('./roll-sim-worker-v30.js?rev=20260918-mutation-2');
 
 // Exact-worker performance guard.
 // Chaska's diminishing returns are a geometric series. The old worker walked
