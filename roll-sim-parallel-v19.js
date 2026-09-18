3ec6c5f68938e9be97c89cac706fbef17a479d00
Drago <dragoidc3@gmail.com>
2026-09-18 01:06:23 -0400

    Refresh active roll simulation worker

--- roll-sim-parallel-v19.js
+++ roll-sim-parallel-v19.js
@@ -199,7 +199,7 @@
       };
 
       for (let slot = 0; slot < concurrency; slot += 1) {
-        const worker = new NativeWorker('./roll-sim-worker-v30.js?rev=20260826-2020');
+        const worker = new NativeWorker('./roll-sim-worker-v30.js?rev=20260918-mutation-2');
         this._workers.push(worker);
         worker.addEventListener('message', (event) => {
           if (failed || this._cancelled) return;
